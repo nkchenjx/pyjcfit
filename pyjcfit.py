749fffbd6c91a8771eec3592a875300bbb788651
Jixin Chen <40338321+nkchenjx@users.noreply.github.com>
2023-11-08 20:59:17 -0600

    Update pyjcfit.py

--- pyjcfit.py
+++ pyjcfit.py
@@ -47,7 +47,7 @@ def pyjcfit(f, xdata, ydata, para_guess, bounds = {}, option = {'maxiteration': 
              error_hist is the square of residual over the iterations.
              gof, goodness of fitting contains R-square, chi-square, sigma of the residual,
                   and bounds of the parameters with 95% confidence
-   
+
     An example is given in the _main_ function.
    
     References
@@ -59,7 +59,7 @@ def pyjcfit(f, xdata, ydata, para_guess, bounds = {}, option = {'maxiteration': 
         Single-Particle Organolead Halide Perovskite Photoluminescence as a Probe for Surface Reaction Kinetics.
         ACS Applied Matierals & Interfaces, 2019, 11(19), 18034-18043.
     """
-    
+
     para = para_guess.copy()
     if len(bounds) == 0:
         ub =  [1E10] * len(para)
@@ -165,26 +165,23 @@ def pyjcfit_goodness(f, xdata, ydata, para, bounds, option):
         # pyplot.scatter(psl[indminl], sigmal[indminl])
         # pyplot.show()
 
-
     goodness_of_fit = {'rsq': rsq, 'chisq': chisq, 'sigma': sigma, 'para95_lb': para95_lb, 'para95_ub': para95_ub}
     return goodness_of_fit
 
 
 # ----------- An example objective function and main function  ---------------:
-
-# import numpy as np
-# import pandas as pd
-# from matplotlib import pyplot
-# from pyjcfit import pyjcfit
-
-def objective(x, para): #must have all parameters in the list para
-    return para[0] * x + para[1]
-
-
-
 if __name__ == '__main__':
-    # load input variables
+    import numpy as np
+    import pandas as pd
+    from matplotlib import pyplot
+    from pyjcfit import pyjcfit
+    import time
 
+    def objective(x, para):
+        return para[0] * x + para[1]
+
+
+    # load input variables
     data = pd.read_csv("data.csv")
     # print(data)
     x_value = data.x
@@ -192,34 +189,40 @@ if __name__ == '__main__':
     #   print(y_value)
 
     # initial guess of parameters and bounds
-    paraguess = [1, 1]  # parameters for the objective function
-    bounds = {'ub': [1000, 1000], 'lb': [-1000, -1000]}  # bounds ordered the same as paramters
-    # check if bounds and paraguess are well ordered.
-    d1 = np.subtract(bounds['ub'], paraguess)
-    d2 = np.subtract(paraguess, bounds['lb'])
+    para_guess = [1, 1]  # initial guessed parameters for the objective function
+    bounds = {'ub': [1000, 1000],
+              'lb': [-1000, -1000]}  # upper bounds (ub) and lower bounds (lb) ordered the same as parameters
+    # check if bounds and para_guess are well-ordered.
+    d1 = np.subtract(bounds['ub'], para_guess)
+    d2 = np.subtract(para_guess, bounds['lb'])
     d3 = np.multiply(d1, d2)
-    d4 = [i for i in d3 if i < 0]
-    if d4:
-        print("bounds unusual, double check bounds and paraguess...")
+    for i in range(len(d3)):
+        if d3[i] < 0:
+            print(" para_guess[%s] and its bounds out of order." % i)
+
     # set searching options
-    option = {'maxiteration': 50, 'precision': 0.0001, 'exp_step': 0.5, 'convgtest': 1E-100}
+    option = {'maxiteration': 50, 'precision': 0.001, 'exp_step': 0.5, 'convgtest': 1E-100}
     # maxiteration is the maximum searching iteration.
     # precision defines the significant figures. It is the smallest numerical search step of each paramter. e.g. paraguess of previous iteration = 10 and precision = 0.01, then searching step is 0.1 for this iteration and this parameter, i.e. precision = 0.01 is 2 sig fig.
     # exp_step, searching step size +-para*precision*(2^exp_step)^n where n is 1, 2, 3,...
     # convgtest is the minimum difference of sum(residual**2) between searching steps to judge convergence.
 
     #  --------- fitting starts
-    fitresults = pyjcfit(objective, x_value, y_value, paraguess, bounds, option)
-    para = fitresults['para']
-    para_hist = fitresults['para_hist']
-    error_hist = fitresults['error_hist']
+    start_time = time.time()
+    fit_results = pyjcfit(objective, x_value, y_value, para_guess, bounds, option)
+    print('\n---- elapsed time for pyjcfit = %f seconds ----' % (time.time() - start_time))
+    para = fit_results['para']
+    para_hist = fit_results['para_hist']
+    error_hist = fit_results['error_hist']
     residual = np.subtract(y_value, objective(x_value, para))
-    goodness_of_fit = fitresults['gof']
-    #  -------_ fitting ends
+    goodness_of_fit = fit_results['gof']
 
-    #------ plot results
+    # ------- fitting ends-----
+
+    # ------ plot results
     print('\nfitted para = ', para)
     print('goodness of fitting: ', goodness_of_fit)
+    print(fit_results['para_hist'])
 
     x_new = np.arange(0, 20, 0.1)
     y_new = objective(x_new, para)
@@ -235,4 +238,5 @@ if __name__ == '__main__':
     pyplot.plot(error_hist, '-', color='red')
     pyplot.title('error history')
     pyplot.show()
+
 # end of example
