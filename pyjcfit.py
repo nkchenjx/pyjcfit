d7cccd529856c4ae22397af5c52064645b38635d
Jixin Chen <40338321+nkchenjx@users.noreply.github.com>
2023-11-08 18:01:13 -0600

    Update pyjcfit.py

--- pyjcfit.py
+++ pyjcfit.py
@@ -29,13 +29,13 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot
 
-def pyjcfit(f, xdata, ydata, paraguess, bounds = {}, option = {'maxiteration': 50, 'precision': 0.00001, 'convgtest': 1E-100}):
-    para = paraguess
+def pyjcfit(f, xdata, ydata, paraguess, bounds = {}, option = {'maxiteration': 50, 'precision': 0.00001, 'exp_step': 0.5, 'convgtest': 1E-100}):
+    para = paraguess.copy()
     if len(bounds) == 0:
-        ub =  [1E10] * len(paraguess)
-        lb = [-1E10] * len(paraguess)
+        ub =  [1E10] * len(para)
+        lb = [-1E10] * len(para)
         bounds = {'ub': ub, 'lb': lb}
-    para_hist = [paraguess] * (option['maxiteration'] + 1)
+    para_hist = [para] * (option['maxiteration'] + 1)
     error_hist = [0] * option['maxiteration']
     errorlast = 0
     for iteration in range(option['maxiteration']):
@@ -50,11 +50,9 @@ def pyjcfit(f, xdata, ydata, paraguess, bounds = {}, option = {'maxiteration': 5
             lb = bounds['lb'][i]
             ub = bounds['ub'][i]
             ll = p - lb
-            nl = range(int(np.floor(np.log2(ll / precision + 1) * 2)), 2, -1)
-            nl = np.divide(list(nl), 2)
+            nl = np.arange(int(np.floor(np.log2(ll / precision + 1))), 1, -option['exp_step'])
             ul = ub - p
-            nu = range(2, int(np.floor(np.log2(ul / precision + 1) * 2)), 1)
-            nu = np.divide(list(nu), 2)
+            nu = np.arange(1, int(np.floor(np.log2(ul / precision + 1))), option['exp_step'])
             ps = np.append(lb, np.subtract(p, np.multiply(np.exp2(nl), precision)))
             ps = np.append(ps, np.add(p, np.multiply(np.exp2(nu), precision)))
             ps = np.append(ps, ub)
@@ -73,19 +71,77 @@ def pyjcfit(f, xdata, ydata, paraguess, bounds = {}, option = {'maxiteration': 5
             break
         errorlast = error[indmin]
     # print(para_hist)
-    fe = pyjcfit_finderror(f, xdata, ydata, para, bounds, option)
-    return {'para': para, 'para_hist': para_hist, 'error_hist': error_hist}
+    gof = pyjcfit_goodness(f, xdata, ydata, para, bounds, option)
+    return {'para': para, 'para_hist': para_hist, 'error_hist': error_hist, 'gof': gof}
 
 
-def pyjcfit_finderror(f, xdata, ydata, para, bounds, option):
+def pyjcfit_goodness(f, xdata, ydata, para, bounds, option):
     yfit = f(xdata, para)
+    n = len(xdata)
     residual = np.subtract(ydata, yfit)
+    residual_sq = np.square(residual, residual)
+    a = np.divide(residual_sq, yfit)
+    chisq = sum(filter(lambda i: not(np.isinf(i)), a))
 
-    return
+    ymean =np.mean(yfit)
+    sumy = sum(np.square(np.subtract(ydata, ymean)))
+    sumr = sum(residual_sq)
+    rsq = 1-sumr/sumy
+    sigma = np.sqrt(sumr/n)
+
+    # find the 95% confidence region of the parameters
+    para95_ub = [0]*len(para)
+    para95_lb = [0]*len(para)
+
+    for i in range(len(para)):
+        precision = option['precision'] * (abs(para[i]) + option['precision'])
+        parau = para.copy()
+        p = para[i]
+        ub = bounds['ub'][i]
+        ul = ub-p
+        nu = np.arange(1, int(np.floor(np.log2(ul / precision + 1))), option['exp_step'])
+        psu = np.append(p, np.add(p, np.multiply(np.exp2(nu), precision)))
+        sigmau = [0]*len(psu)
+        for j in range(len(psu)):
+            parau[i] = psu[j]
+            yfitu = f(xdata, parau)
+            residual = np.subtract(ydata, yfitu)
+            sigmau[j] = np.sqrt(np.dot(residual, residual)/n)
+        a = abs(np.subtract(sigmau , sigma))
+        b = abs(a-2*sigma/np.sqrt(n-len(para)))
+        indminu = np.array(b).argmin()
+        para95_ub[i] = psu[indminu]
+
+        paral = para.copy()
+        lb = bounds['lb'][i]
+        ll = p - lb
+        nl = np.arange(int(np.floor(np.log2(ll / precision + 1))), 1, -option['exp_step'])
+        psl = np.append(np.subtract(p, np.multiply(np.exp2(nl), precision)), p)
+        sigmal = [0] * len(psl)
+        for j in range(len(psl)):
+            paral[i] = psl[j]
+            yfitl = f(xdata, paral)
+            residual = np.subtract(ydata, yfitl)
+            sigmal[j] = np.sqrt(np.dot(residual, residual)/n)
+        a = abs(sigmal - sigma)
+        b = abs(a-2*sigma/np.sqrt(n-len(para)))
+        indminl = np.array(b).argmin()
+        para95_lb[i] = psl[indminl]
+
+        # # show the error vs parameter curve
+        # pyplot.plot(psu, sigmau)
+        # pyplot.plot(psl, sigmal)
+        # pyplot.scatter(psu[indminu], sigmau[indminu])
+        # pyplot.scatter(psl[indminl], sigmal[indminl])
+        # pyplot.show()
+
+
+    goodness_of_fit = {'rsq': rsq, 'chisq': chisq, 'sigma': sigma, 'para95_lb': para95_lb, 'para95_ub': para95_ub}
+    return goodness_of_fit
 
 
 # ----------- An example objective function and main function  ---------------:
-def objective(x, para):
+def objective(x, para): #must have all parameters in the list para
     return para[0] * x + para[1]
 
 
@@ -107,11 +163,12 @@ if __name__ == '__main__':
     d3 = np.multiply(d1, d2)
     d4 = [i for i in d3 if i < 0]
     if d4:
-        print("bounds unusual")
+        print("bounds unusual, double check bounds and paraguess...")
     # set searching options
-    option = {'maxiteration': 50, 'precision': 0.0001, 'convgtest': 1E-100}
+    option = {'maxiteration': 50, 'precision': 0.0001, 'exp_step': 0.5, 'convgtest': 1E-100}
     # maxiteration is the maximum searching iteration.
     # precision defines the significant figures. It is the smallest numerical search step of each paramter. e.g. paraguess of previous iteration = 10 and precision = 0.01, then searching step is 0.1 for this iteration and this parameter, i.e. precision = 0.01 is 2 sig fig.
+    # exp_step, searching step size +-para*precision*(2^exp_step)^n where n is 1, 2, 3,...
     # convgtest is the minimum difference of sum(residual**2) between searching steps to judge convergence.
 
     #  --------- fitting starts
@@ -120,11 +177,14 @@ if __name__ == '__main__':
     para_hist = fitresults['para_hist']
     error_hist = fitresults['error_hist']
     residual = np.subtract(y_value, objective(x_value, para))
+    goodness_of_fit = fitresults['gof']
     #  -------_ fitting ends
 
-    # plot results
-    x_new = np.arange(0, 20, 0.1)
+    #------ plot results
     print('\nfitted para = ', para)
+    print('goodness of fitting: ', goodness_of_fit)
+
+    x_new = np.arange(0, 20, 0.1)
     y_new = objective(x_new, para)
 
     pyplot.subplot(1, 2, 1)
